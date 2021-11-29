1dfede4f110d4f878f614b560b25f93dd6ea99f9
Mubai Hua <hmbyxfyj@gmail.com>
2021-11-28 17:40:56 -0800

    update signup model to store comments

--- Server/models/SignUpModels.js
+++ Server/models/SignUpModels.js
@@ -1,29 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const signUpTemplate = new mongoose.Schema({
-    userName:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    }, 
-    finished_order_number: {
-        type: Number,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    }
-})
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  finished_order_number: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  comment: {
+    type: [
+      {
+        rating: Number,
+        content: { type: String, default: "That user didn't post a comment" },
+      },
+    ],
+    required: false,
+  },
+});
 
-module.exports = mongoose.model('myusernames', signUpTemplate)
+module.exports = mongoose.model("myusernames", signUpTemplate);
